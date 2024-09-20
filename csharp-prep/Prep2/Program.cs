750cce9e03247bbbd9335ba096d5f2f180a4b299
vinickchristian <vinickmiamo19@gmail.com>
2024-09-20 15:24:56 +0100

    prep

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade:");
+        string gradePercentage = Console.ReadLine();
+        int grade = int.Parse(gradePercentage);
+        
+        string laterGrade = "";
+        if (grade >= 90)
+        {
+             laterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+             laterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+             laterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+             laterGrade = "D";
+        }
+        else 
+        {
+              laterGrade = "F";
+        }
+        
+        Console.WriteLine($"Your letter grade is {laterGrade}.");
+
+        if (grade >=70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else 
+        {
+            Console.WriteLine("Sorry, you failed the course.");
+        }
     }
 }
