2f7e7e4ebc1f94d43f5eb318060ebfc69c430776
vinickchristian <vinickmiamo19@gmail.com>
2024-09-21 16:44:28 +0100

    using the if

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+
+        if (guess > magicNumber)
+        {
+            Console.WriteLine("higher");
+        }
+        else if (guess < magicNumber)
+        {
+            Console.WriteLine("lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
     }
 }
